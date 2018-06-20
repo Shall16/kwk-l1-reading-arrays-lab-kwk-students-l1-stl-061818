f99a68c2003fec3cb40ae55b564ca83e6bbb6bee
Shall16 <shall130@rsdmo.org>
2018-06-20 16:38:30 +0000

    automatically backed up by learn

---
+++ array.rb
@@ -0,0 +1,16 @@
+disney_array = ["Tiana","Mulan","Sebastian","Pua","Fat louie"]
+disney_array.push ("Pooh Bear")
+disney_array.push ("Tiger")
+
+puts disney_array
+puts disney_array.length
+
+# table_ages = [14,15,13,14]
+
+# puts table_ages
+
+# favorite_books = ["A court of thorns and roses", "The Nightingale"," Percy Jackson Greek Gods"]
+
+# # puts favorite_books [0]
+# favorite_books.push("Where ever You Are")
+# puts favorite_books 
--- reading_arrays.rb
+++ reading_arrays.rb
@@ -6,27 +6,26 @@ STUDENT_NAMES = [
   "Nicki Minaj",
   "Rihanna"
 ]
-
 def first_student_by_index
-  # Write a solution that returns the first student in the array student_names
+  puts STUDENT_NAMES [0]
 end
+first_student_by_index
 
-def fourth_student_by_index
-  # Write a solution that returns the first student in the array student_names
-end
+# def fourth_student_by_index
+#   puts fourth_student_by_index[4]
+# end
+# first_student_by_index
+# def last_student_by_index
+# end
 
-def last_student_by_index
-  # Write a solution that returns the first student in the array student_names
-end
+# def first_student_by_method
+#   # Write a solution that returns the first student using the built in .first method
+# end
 
-def first_student_by_method
-  # Write a solution that returns the first student using the built in .first method
-end
+# def last_student_by_method
+#   # Write a solution that returns the first student using the built in .last method
+# end
 
-def last_student_by_method
-  # Write a solution that returns the first student using the built in .last method
-end
-
-def first_second_and_third_students
-  # Write a solution that returns the first, second and third students
-end
+# def first_second_and_third_students
+#   # Write a solution that returns the first, second and third students
+# end
